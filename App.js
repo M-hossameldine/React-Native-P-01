dc926bb0cfcb98f375b0447fc414fc027e7c7177
M-hossameldine <m.hossameldine@gmail.com>
2025-07-05 21:01:16 +0300

    feature: imporve the layout using Flexbox

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
         <TextInput placeholder="Enter your goal!" style={styles.textInput} />
         <Button title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -16,17 +16,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
     gap: 8,
+    paddingBottom: 16,
+    marginBottom: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#ccc",
-    width: "80%",
+    width: "70%",
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 1,
   },
 });
