ab521d6e98f976d5cc474546405737945b701a22
M-hossameldine <m.hossameldine@gmail.com>
2025-07-05 21:21:56 +0300

    feature: handle adding and listing user goals

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
+import { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [userGoals, setUserGoals] = useState([]);
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    if (enteredGoalText.trim().length === 0) {
+      return;
+    }
+
+    setUserGoals((currentUserGoals) => {
+      setEnteredGoalText("");
+      return [enteredGoalText, ...currentUserGoals];
+    });
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Enter your goal!" style={styles.textInput} />
-        <Button title="Add Goal" />
+        <TextInput
+          placeholder="Enter your goal!"
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
+        {userGoals.map((goal, index) => (
+          <Text key={index}>{goal}</Text>
+        ))}
       </View>
     </View>
   );
